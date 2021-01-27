f5f3c375b9c48cb93ef6f2429ebb66d53e5e25cd
time-killer-games <samuelvenable@hotmail.com>
2021-01-27 13:07:23 -0500

    Update pawgui_file_popups.cpp

--- src/pawgui/pawgui_file_popups.cpp
+++ src/pawgui/pawgui_file_popups.cpp
@@ -32,10 +32,10 @@ SOFTWARE.
 */
 
 #if defined(_WIN32) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__)
-#include <cstring>
-#include <SDL2/SDL_syswm.h>
-#include "../other_libs/dlgmodule.h"
-#include "../sdl_libs/gpe_window_controller_sdl.h"
+    #include <cstring>
+    #include <SDL2/SDL_syswm.h>
+    #include "../other_libs/dlgmodule.h"
+    #include "../sdl_libs/gpe_window_controller_sdl.h"
 #endif
 
 #include "pawgui_file_popups.h"
@@ -59,30 +59,34 @@ namespace pawgui
 
     std::string get_filename_plain_from_popup( const std::string & prompt, std::string allowedFileTypes, std::string &previousDirectory,bool isSaving, bool isDirectorySearch )
     {
-    std::string returnFile;
-    #if defined(_WIN32) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__)
-            if (!isDirectorySearch) {
+        std::string returnFile;
+        #if defined(_WIN32) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__)
+            if (!isDirectorySearch)
+            {
                 std::string osFileFilterString = gpe::parse_file_types(allowedFileTypes);
                 #if defined(_WIN32) || (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__)
-                SDL_SysWMinfo wmInfo;
-                SDL_VERSION(&wmInfo.version);
-                SDL_GetWindowWMInfo(gpe::window_controller_main_sdl->get_sdl_window(), &wmInfo);
-                #if defined(_WIN32)
-                widget_set_owner((char *)std::to_string((unsigned long long)wmInfo.info.win.window).c_str());
-                #elif (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__)
-                widget_set_owner((char *)std::to_string((unsigned long long)wmInfo.info.x11.window).c_str());
-                #endif
+                    SDL_SysWMinfo wmInfo;
+                    SDL_VERSION(&wmInfo.version);
+                    SDL_GetWindowWMInfo(gpe::window_controller_main_sdl->get_sdl_window(), &wmInfo);
+                    #if defined(_WIN32)
+                        widget_set_owner((char *)std::to_string((unsigned long long)wmInfo.info.win.window).c_str());
+                    #elif (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__)
+                        widget_set_owner((char *)std::to_string((unsigned long long)wmInfo.info.x11.window).c_str());
+                    #endif
                 #endif
                 widget_set_icon((char *)"icon.png");
                 if (!osFileFilterString.empty() && osFileFilterString.back() == '|')
                     osFileFilterString.pop_back();
-                if (!isSaving) {
+                if (!isSaving)
+                {
                     returnFile = get_open_filename_ext((char *)osFileFilterString.c_str(), (char *)"", (char *)previousDirectory.c_str(), (char *)prompt.c_str());
-                } else {
+                }
+                else
+                {
                     returnFile = get_save_filename_ext((char *)osFileFilterString.c_str(), (char *)"", (char *)previousDirectory.c_str(), (char *)prompt.c_str());
                 }
             }
-    #endif
+        #endif
         return returnFile;
     }
 }
