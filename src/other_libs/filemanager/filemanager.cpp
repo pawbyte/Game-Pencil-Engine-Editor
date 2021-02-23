fcb6aa82bdda9826f880e5f78a31fb1269f6c50c
time-killer-games <samuelvenable@hotmail.com>
2021-02-23 16:40:56 -0500

    Update filemanager.cpp

--- src/other_libs/filemanager/filemanager.cpp
+++ src/other_libs/filemanager/filemanager.cpp
@@ -394,7 +394,7 @@ namespace tkg {
   }
 
   vector<string> filemanager::directory_contents(string dname, string pattern) {
-    std::error_code ec; vector<string> result;
+    std::error_code ec; vector<string> itemvec, result;
     if (pattern.empty()) pattern = "*.*";
     if (!directory_exists(dname)) return result;
     dname = filename_remove_slash(dname, true);
@@ -405,9 +405,9 @@ namespace tkg {
         if (ec.value() != 0) { break; }
         fs::path file_path = fs::u8path(filename_canonical(dir_ite->path().u8string()));
         if (!fs::is_directory(dir_ite->status(ec)) && ec.value() == 0) {
-          itemvec.pop_back(file_path.u8string());
+          itemvec.push_back(file_path.u8string());
         } else if (ec.value() == 0) {
-          itemvec.pop_back(filename_add_slash(file_path.u8string()));
+          itemvec.push_back(filename_add_slash(file_path.u8string()));
         }
       }
     }
@@ -427,7 +427,6 @@ namespace tkg {
     for (string filtered_name : filtered_items) {
       result.push_back(filtered_name);
     }
-    result.pop_back();
     return result;
   }
 
