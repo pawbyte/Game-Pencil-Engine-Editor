f0b738a10e0a40b14c907fd8bf9b69147a577f31
nhurde <theweirdn8@yahoo.com>
2024-05-03 12:36:14 -0500

    [ISU-123] Implement object list and map add/remove based on object types.

--- src/gpe/gpe_layer.cpp
+++ src/gpe/gpe_layer.cpp
@@ -33,6 +33,7 @@ SOFTWARE.
 
 #include "gpe_layer.h"
 #include "gpe_runtime.h"
+#include <functional>
 
 namespace gpe
 {
@@ -73,6 +74,52 @@ namespace gpe
         if( new_branch == nullptr )
         {
             return;
+        }
+
+        if( new_branch->get_type() == branch_type::OBJECT )
+        {
+            add_game_object( (game_object *) new_branch );
+        }
+    }
+
+
+    void scene_layer::add_game_object ( game_object * obj_to_add )
+    {
+        if( obj_to_add == nullptr)
+        {
+            return;
+        }
+        add_game_object_to_list( obj_to_add );
+        add_game_object_to_map( obj_to_add );
+
+    }
+
+    void scene_layer::add_game_object_to_list ( game_object * obj_to_add )
+    {
+        if( obj_to_add == nullptr)
+        {
+            return;
+        }
+        layer_entities.push_back( obj_to_add );
+    }
+
+    void scene_layer::add_game_object_to_map ( game_object * obj_to_add )
+    {
+        if( obj_to_add == nullptr)
+        {
+            return;
+        }
+
+        auto it = object_layer_map.find( obj_to_add->get_object_type() );
+        if (it != object_layer_map.end())
+        {
+            it->second.push_back( obj_to_add );
+        }
+        else
+        {
+            std::vector< game_object * > new_object_vector;
+            new_object_vector.push_back( obj_to_add );
+            object_layer_map.insert( std::make_pair( obj_to_add->get_object_type(), new_object_vector) );
         }
     }
 
@@ -144,17 +191,85 @@ namespace gpe
         return layer_id;
     }
 
-    void scene_layer::remove_object ( game_object * objToRemove)
+    int scene_layer::get_object_count( int object_type_id )
     {
-        game_object *  tObject = nullptr;
+        if (object_layer_map.count(object_type_id) > 0)
+        {
+            return 0;
+        }
+        const std::vector<game_object*>& found_objects = object_layer_map.at(object_type_id);
+        return found_objects.size();
+    }
+
+    std::vector<game_object * >  scene_layer::get_object_list(int object_type_id )
+    {
+        if (object_layer_map.count( object_type_id ) > 0)
+        {
+          std::vector<game_object*>& objects = object_layer_map.at(object_type_id);
+          return objects;
+        }
+        std::vector<game_object*> empty_vector(0, 0);
+        return empty_vector;
+    }
+
+    int scene_layer::remove_object ( game_object * obj_to_remove)
+    {
+        if( obj_to_remove == nullptr)
+        {
+            return -2; //Object not removed due to nullptr
+        }
+        bool removed_from_list = remove_object_from_list( obj_to_remove );
+        bool removed_from_map = remove_object_from_map( obj_to_remove );
+
+        if( removed_from_list && removed_from_map )
+        {
+            return 1;
+        }
+        else if( removed_from_list || removed_from_map )
+        {
+            return 0; //Was able to remove from list or map, but not both
+        }
+        return -1; //Was not able to remove from either...
+    }
+
+    bool scene_layer::remove_object_from_list ( game_object * obj_to_remove)
+    {
+        if( obj_to_remove == nullptr)
+        {
+            return false;
+        }
+        game_object *  temo_object = nullptr;
         for( int iItr = (int)layer_entities.size()-1; iItr >=0; iItr--)
         {
-            tObject = layer_entities[iItr];
-            if( tObject->get_id()==objToRemove->get_id() )
+            temo_object = layer_entities[iItr];
+            if( temo_object->get_id()==obj_to_remove->get_id() )
             {
                 layer_entities.erase( layer_entities.begin()+iItr );
+                return true;
             }
         }
+        return false;
+    }
+
+    bool scene_layer::remove_object_from_map ( game_object * obj_to_remove)
+    {
+        if( obj_to_remove == nullptr)
+        {
+            return false;
+        }
+
+        int object_type_seeked = obj_to_remove->get_object_type();
+        if (object_layer_map.count( object_type_seeked ) > 0)
+        {
+          std::vector<game_object*>& objects = object_layer_map.at(object_type_seeked);
+          int past_size = (int)objects.size();
+          objects.erase(std::remove(objects.begin(), objects.end(), obj_to_remove), objects.end());
+          if ( (int)objects.size() != past_size)
+          {
+            return true;
+          }
+        }
+        return false;
     }
 
     void scene_layer::render()
@@ -184,6 +299,18 @@ namespace gpe
     }
 
 
+    //Layer New Background functions added as of Version 1.12 [ BEGIN ]
+    void scene_layer::replace_all_backgrounds ( int needleBgId, int newBgId )
+    {
+        for( int i = (int)layer_backgrounds.size()-1; i >=0; i-- )
+        {
+            if( layer_backgrounds[i]->get_background_id() == needleBgId )
+            {
+                layer_backgrounds[i]->set_background( newBgId );
+            }
+        }
+    }
+
     void scene_layer::reset_branch()
     {
         branch::reset_branch();
@@ -202,17 +329,6 @@ namespace gpe
         }
     }
 
-    //Layer New Background functions added as of Version 1.12 [ BEGIN ]
-    void scene_layer::replace_all_backgrounds ( int needleBgId, int newBgId )
-    {
-        for( int i = (int)layer_backgrounds.size()-1; i >=0; i-- )
-        {
-            if( layer_backgrounds[i]->get_background_id() == needleBgId )
-            {
-                layer_backgrounds[i]->set_background( newBgId );
-            }
-        }
-    }
 
     void scene_layer::set_layer_background ( int layerBeingChanged, int bg_id, int newBgId )
     {
@@ -220,6 +336,11 @@ namespace gpe
         {
             layer_backgrounds[layerBeingChanged]->set_background( newBgId );
         }
+    }
+
+    void sorth_depth( bool sort_ascending )
+    {
+
     }
 
     void scene_layer::update(float delta_time)
